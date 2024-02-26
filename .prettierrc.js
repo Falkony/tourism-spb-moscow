62a14a8ff02bbf5ff014c6fe50c6399fbf053c43
Falkony <max.spiridonov@gmail.com>
2024-02-26 16:12:33 +0300

    добавлены базовые настройки

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    printWidth: 120,
+    semi: true,
+    tabWidth: 4,
+    endOfLine: 'auto',
+    singleQuote: true,
+    singleAttributePerLine: true
+};
---
+++ src/views/MainPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MainPage from '@/components/Index/MainPage.vue';
+</script>
+
+<template>
+    <div>
+        <MainPage />
+    </div>
+</template>
