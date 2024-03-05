62cc3549d3d95bf9d9a8b0fd0c7ae75e903618d5
Falkony <max.spiridonov@gmail.com>
2024-03-05 10:39:31 +0300

    Добавлено хранилище pinia

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import i18n from '../i18n';
 import App from './App.vue';
 import './assets/index.css';
 import router from './router';
 
+const pinia = createPinia();
 const app = createApp(App);
 
-app.use(router).use(i18n).mount('#app');
+app.use(router).use(i18n).use(pinia).mount('#app');
