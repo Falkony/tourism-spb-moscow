2ceb5c63b02d122d57113f26f2f158ea48f60bc1
Falkony <max.spiridonov@gmail.com>
2024-03-20 01:11:16 +0300

    agents

--- src/views/Agents.vue
+++ src/views/Agents.vue
@@ -1,7 +1,34 @@
 <script setup lang="ts">
-import MainPage from '@/components/Index/MainPage.vue';
+import BaseButton from '@/components/common/BaseButton.vue';
+import BaseTypography from '@/components/common/BaseTypography.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 </script>
 
 <template>
-    <MainPage />
+    <div class="flex-1 mt-[189px]">
+        <div class="flex items-center w-full mb-[76px]">
+            <div
+                class="pl-[120px] pr-[76px] cursor-pointer"
+                @click="router.back()"
+            >
+                <img src="@/assets/svg/arrow-left.svg" />
+            </div>
+
+            <BaseButton
+                text="Агентам"
+                type="subtitle"
+                ui="primary"
+                color="var(--black-color)"
+            />
+        </div>
+
+        <div class="max-w-[1126px] mx-auto">
+            <BaseTypography
+                text="Страница в разработке"
+                type="subtitle2"
+            />
+        </div>
+    </div>
 </template>
