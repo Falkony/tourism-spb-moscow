c0c6b1c7aaeb993404ce5821f7680010e9f1de11
Falkony <max.spiridonov@gmail.com>
2024-04-03 02:07:56 +0300

    страница тура развернутая

--- src/components/Tours/Diagram.vue
+++ src/components/Tours/Diagram.vue
@@ -1,30 +1,178 @@
-<script setup lang="ts"></script>
-
+<script lang="ts" setup>
+import BaseTypography from '@/components/common/BaseTypography.vue';
+</script>
 <template>
     <div
-        class="relative flex flex-wrap gap-x-[45px] gap-y-[30px] items-center mt-[40px] px-[20px] max-w-[1126px] mx-auto w-full mb-[104px]"
+        class="relative flex flex-wrap gap-x-[45px] gap-y-[30px] items-center px-[20px] max-w-[1126px] mx-auto w-full mb-[104px]"
     >
-        <div class="block"></div>
-        <div class="circle"></div>
-        <div class="circle"></div>
-        <div class="block"></div>
+        <!-- Day 1 -->
+        <div class="block px-10 py-6">
+            <div class="flex flex-col gap-y-3 w-full">
+                <BaseTypography
+                    text="День 1"
+                    type="special-body"
+                    color="var(--primary-color)"
+                />
+
+                <ul class="list-disc pl-5">
+                    <li>
+                        <BaseTypography
+                            text="Обзорная экскурсия в Петропавловскую крепость (собор + Трубецкой бастион)."
+                            type="special-body4"
+                        />
+                    </li>
+
+                    <li>
+                        <BaseTypography
+                            text="Экскурсия в музей-макет “Петровская акватория”."
+                            type="special-body4"
+                        />
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="circle">
+            <img src="@/assets/images/tours/mini/1.png" />
+        </div>
+
+        <div class="circle">
+            <img src="@/assets/images/tours/mini/2.png" />
+        </div>
+
+        <!-- Day 2 -->
+        <div class="block px-10 py-6">
+            <div class="flex flex-col gap-y-3 w-full">
+                <BaseTypography
+                    text="День 2"
+                    type="special-body"
+                    color="var(--primary-color)"
+                />
+
+                <ul class="list-disc pl-5">
+                    <li>
+                        <BaseTypography
+                            text="Экскурсия в Исаакиевский собор."
+                            type="special-body4"
+                        />
+                    </li>
+
+                    <li>
+                        <BaseTypography
+                            text="Выезд в Царское село. Экскурсия в Екатерининский дворец."
+                            type="special-body4"
+                        />
+                    </li>
+                </ul>
+            </div>
+        </div>
 
         <div class="flex gap-x-[45px] mr-[150px] ml-auto items-center flex-row-reverse justify-end">
-            <div class="circle"></div>
-            <div class="circle"></div>
-            <div class="block"></div>
+            <div class="circle">
+                <img src="@/assets/images/tours/mini/3.png" />
+            </div>
+
+            <div class="circle">
+                <img src="@/assets/images/tours/mini/4.png" />
+            </div>
+
+            <!-- Day 3 -->
+            <div class="block px-10 py-6">
+                <div class="flex flex-col gap-y-3 w-full">
+                    <BaseTypography
+                        text="День 3"
+                        type="special-body"
+                        color="var(--primary-color)"
+                    />
+
+                    <ul class="list-disc pl-5">
+                        <li>
+                            <BaseTypography
+                                text="Отправление в Петергоф. Экскурсия по Нижнему парку с посещением Большого дворца и малого музея."
+                                type="special-body4"
+                            />
+                        </li>
+
+                        <li>
+                            <BaseTypography
+                                text="Экскурсия в музей Фаберже."
+                                type="special-body4"
+                            />
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
 
         <div class="w-[250px]"></div>
-        <div class="circle mt-[-20px]"></div>
-        <div class="circle mt-[-20px]"></div>
-        <div class="block mt-[-20px]"></div>
+
+        <div class="circle mt-[20px]">
+            <img src="@/assets/images/tours/mini/5.png" />
+        </div>
+
+        <div class="circle mt-[20px]">
+            <img src="@/assets/images/tours/mini/6.png" />
+        </div>
+
+        <!-- Day 4 -->
+        <div class="block px-10 py-6 mt-[20px] !h-[222px]">
+            <div class="flex flex-col gap-y-3 w-full">
+                <BaseTypography
+                    text="День 4"
+                    type="special-body"
+                    color="var(--primary-color)"
+                />
+
+                <ul class="list-disc pl-5">
+                    <li>
+                        <BaseTypography
+                            text="Выезд в Кронштадт."
+                            type="special-body4"
+                        />
+                    </li>
+
+                    <li>
+                        <BaseTypography
+                            text="Посещение с экскурсией музея Эрмитаж."
+                            type="special-body4"
+                        />
+                    </li>
+                </ul>
+            </div>
+        </div>
 
         <div class="flex gap-x-[45px] mr-[150px] ml-auto items-center flex-row-reverse justify-end">
-            <div class="circle mt-[-10px]"></div>
-            <div class="circle mt-[-10px]"></div>
-            <div class="circle mt-[-10px]"></div>
-            <div class="block mt-[-10px]"></div>
+            <div class="circle mt-[86px]">
+                <img src="@/assets/images/tours/mini/7.png" />
+            </div>
+
+            <div class="circle mt-[86px]">
+                <img src="@/assets/images/tours/mini/8.png" />
+            </div>
+
+            <div class="circle mt-[86px]">
+                <img src="@/assets/images/tours/mini/9.png" />
+            </div>
+
+            <!-- Day 5 -->
+            <div class="block px-10 py-6 mt-[86px] !h-[170px]">
+                <div class="flex flex-col gap-y-3 w-full">
+                    <BaseTypography
+                        text="День 5"
+                        type="special-body"
+                        color="var(--primary-color)"
+                    />
+
+                    <ul class="list-disc pl-5">
+                        <li>
+                            <BaseTypography
+                                text="Экскурсия в музей-усадьбу Державина"
+                                type="special-body4"
+                            />
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
 
         <img
@@ -51,5 +199,11 @@
 
 .circle {
     @apply h-[148px] w-[148px] rounded-full bg-slate-200 z-10;
+}
+
+.circle > img {
+    object-fit: cover;
+    display: block;
+    width: 100%;
 }
 </style>
---
+++ src/components/Tours/TourDesktop.vue
@@ -0,0 +1,296 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import ReturnButton from '@/components/common/ReturnButton.vue';
+import BaseTypography from '@/components/common/BaseTypography.vue';
+import BaseButton from '@/components/common/BaseButton.vue';
+import BaseAccordion from '@/components/common/BaseAccordion.vue';
+import Diagram from './Diagram.vue';
+
+const toggle = ref<boolean>(false);
+
+const toggleForm = () => (toggle.value = !toggle.value);
+</script>
+
+<template>
+    <div class="flex flex-col flex-1 mt-[189px]">
+        <div class="flex max-w-[1126px] mx-auto w-full mb-[76px]">
+            <ReturnButton text="Бессмертная классика" />
+
+            <div
+                class="flex items-center border-[3px] border-[var(--primary-color)] rounded-[100px] px-[24.5px] py-[10px] w-fit ml-auto"
+            >
+                <BaseTypography
+                    text="5 дней"
+                    type="subtitle2"
+                    class="whitespace-nowrap"
+                />
+            </div>
+        </div>
+
+        <div class="reservation">
+            <div class="flex items-center gap-x-[88px]">
+                <div>
+                    <BaseTypography
+                        text="XXX "
+                        type="subtitle"
+                        color="var(--secondary-color)"
+                        tag="span"
+                    />
+
+                    <BaseTypography
+                        text="₽/чел"
+                        type="body2"
+                        tag="span"
+                    />
+
+                    <BaseTypography
+                        text="группа (2-6 чел)"
+                        type="body"
+                    />
+                </div>
+
+                <div>
+                    <BaseTypography
+                        text="XXX "
+                        type="subtitle"
+                        color="var(--secondary-color)"
+                        tag="span"
+                    />
+
+                    <BaseTypography
+                        text="₽/чел"
+                        type="body2"
+                        tag="span"
+                    />
+
+                    <BaseTypography
+                        text="индивидуальная"
+                        type="body"
+                    />
+                </div>
+            </div>
+
+            <BaseButton
+                text="Забронировать"
+                padding-y="20"
+                padding-x="64"
+                type="subtitle4"
+                color="var(--base-white)"
+                ui="primary-with-back"
+                @click="toggleForm"
+            />
+        </div>
+
+        <div class="flex max-w-[1126px] mx-auto p-3 w-full mb-[76px]">
+            <BaseTypography
+                text="Программа"
+                type="subtitle"
+            />
+        </div>
+
+        <Diagram />
+
+        <div class="flex flex-col max-w-[1126px] mx-auto w-full">
+            <section class="mb-[104px]">
+                <div class="p-3 mb-[76px]">
+                    <BaseTypography
+                        text="Что включено"
+                        type="subtitle"
+                    />
+                </div>
+
+                <BaseAccordion
+                    title="Экскурсионное обслуживание"
+                    class="mb-[30px]"
+                    disabled
+                />
+
+                <BaseAccordion
+                    title="Входные билеты"
+                    disabled
+                />
+            </section>
+
+            <section class="mb-[104px]">
+                <div class="p-3 mb-[76px]">
+                    <BaseTypography
+                        text="Дополнительно"
+                        type="subtitle"
+                    />
+                </div>
+
+                <BaseAccordion
+                    title="Проживание"
+                    class="mb-[30px]"
+                    disabled
+                />
+
+                <BaseAccordion
+                    title="Питание"
+                    disabled
+                />
+            </section>
+
+            <section class="mb-[104px]">
+                <div class="p-3 mb-[76px]">
+                    <BaseTypography
+                        text="Часто задаваемые вопросы"
+                        type="subtitle"
+                    />
+                </div>
+
+                <BaseAccordion
+                    title="Как забронировать и оплатить"
+                    class="mb-[30px]"
+                >
+                    <template #default>
+                        <div class="pl-5 mt-4">
+                            <BaseTypography
+                                text="Шаг 1: отправить заявку"
+                                type="body2"
+                                class="mb-6"
+                            />
+
+                            <BaseTypography
+                                text="Забронировать тур вы можете следующим образом:"
+                                type="body"
+                                class="mb-6"
+                            />
+
+                            <BaseTypography
+                                text="- нажать кнопку «ЗАБРОНИРОВАТЬ» в описании экскурсии;"
+                                type="body"
+                            />
+                            <BaseTypography
+                                text="- позвонить по телефону +7 (812) 207-03-50;"
+                                type="body"
+                            />
+                            <BaseTypography
+                                text="- отправить запрос по электронной почте info@rvm-spb.ru."
+                                type="body"
+                                class="mb-6"
+                            />
+
+                            <BaseTypography
+                                text="Шаг 2: оплатить билеты"
+                                type="body2"
+                                class="mb-6"
+                            />
+
+                            <BaseTypography
+                                text="У вас есть 2 способа сделать это:"
+                                type="body"
+                                class="mb-6"
+                            />
+
+                            <BaseTypography
+                                text="1) Удалённо, через различные системы оплат (расчет с использованием реквизитов банковской карты, переводом через интернет-банк)."
+                                type="body"
+                            />
+
+                            <BaseTypography
+                                text="2) Подъехать заранее к нам в офис и оплатить наличными или по картам VISA, Mastercard, МИР. Наш офис в Бизнес центре 'СИТИ', ул. Садовая д.12, офис № 3 на 3 этаже."
+                                type="body"
+                                class="mb-6"
+                            />
+
+                            <div>
+                                <BaseTypography
+                                    text="Внимание! "
+                                    type="body2"
+                                    tag="span"
+                                />
+
+                                <BaseTypography
+                                    text="Заявка на тур подтверждается только специалистом компании."
+                                    type="body"
+                                    tag="span"
+                                />
+                            </div>
+                        </div>
+                    </template>
+                </BaseAccordion>
+
+                <BaseAccordion
+                    title="Способ оплаты"
+                    class="mb-[30px]"
+                >
+                    <template #default>
+                        <ul class="list-disc pl-10 mt-4">
+                            <li>
+                                <BaseTypography
+                                    text="Visa"
+                                    type="body"
+                                />
+                            </li>
+                            <li>
+                                <BaseTypography
+                                    text="MasterCard"
+                                    type="body"
+                                />
+                            </li>
+                            <li>
+                                <BaseTypography
+                                    text="Сбербанк"
+                                    type="body"
+                                />
+                            </li>
+                            <li>
+                                <BaseTypography
+                                    text="ВТБ"
+                                    type="body"
+                                />
+                            </li>
+                            <li>
+                                <BaseTypography
+                                    text="Тинькофф"
+                                    type="body"
+                                />
+                            </li>
+                            <li>
+                                <BaseTypography
+                                    text="Наличными"
+                                    type="body"
+                                />
+                            </li>
+                        </ul>
+                    </template>
+                </BaseAccordion>
+
+                <BaseAccordion
+                    title="Сроки оплаты тура"
+                    class="mb-[30px]"
+                >
+                    <template #default>
+                        <div class="pl-5 mt-4">
+                            <BaseTypography
+                                text="Сроки оплаты тура указываются в договоре, который заключается после подтверждении бронирования тура."
+                                type="body"
+                            />
+                        </div>
+                    </template>
+                </BaseAccordion>
+
+                <BaseAccordion
+                    title="Условия отмены бронирования"
+                    class="mb-[30px]"
+                >
+                    <template #default>
+                        <div class="pl-5 mt-4">
+                            <BaseTypography
+                                text="Условия отмены бронирования указываются в договоре, который заключается после подтверждении бронирования тура."
+                                type="body"
+                            />
+                        </div>
+                    </template>
+                </BaseAccordion>
+            </section>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.reservation {
+    @apply flex gap-x-[132px] items-center border-[3px] border-[var(--primary-color)] px-[104px] py-9 max-w-[1126px] mx-auto rounded-[100px] mb-[104px];
+}
+</style>
