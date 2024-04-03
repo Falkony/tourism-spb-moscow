91c5c39dc0a26077484d33ae9731b834f3babb4c
Falkony <max.spiridonov@gmail.com>
2024-04-03 14:13:00 +0300

    лишний код

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { createYmaps } from 'vue-yandex-maps';
 import i18n from '../i18n';
 import App from './App.vue';
 import './assets/index.css';
@@ -8,8 +7,5 @@ import router from './router';
 
 const pinia = createPinia();
 const app = createApp(App);
-const ymaps = createYmaps({
-    apikey: '79568a14-1219-4ca9-b408-38d9bd05c8bd',
-});
 
-app.use(router).use(i18n).use(pinia).use(ymaps).mount('#app');
+app.use(router).use(i18n).use(pinia).mount('#app');
