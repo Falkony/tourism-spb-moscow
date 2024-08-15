e51db2ab2c3eba26f4609a792aac7ded0030219d
Falkony <max.spiridonov@gmail.com>
2024-08-16 01:20:03 +0300

    страница круизов

--- src/components/Tours/Category.vue
+++ src/components/Tours/Category.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
-import { toRefs, onMounted, ref, nextTick } from 'vue';
-import { useI18n } from 'vue-i18n';
+import { ref, onMounted, nextTick } from 'vue';
 import { useRouter } from 'vue-router';
 import { useGlobalStore } from '@/stores/global';
 import SlideItem from '@/components/common/SlideItem.vue';
 import BaseReturn from '@/components/common/BaseReturn.vue';
 import BaseTypography from '@/components/common/BaseTypography.vue';
 
-const { t } = useI18n();
 const router = useRouter();
 
 const id = +router.currentRoute.value.params.id;
 
 const globalStore = useGlobalStore();
-const { category, isMobile } = toRefs(globalStore);
+const { category, isMobile } = globalStore;
 const { getCategory } = globalStore;
 
-const widgetDataId = ref<string>(
-    'YTo0OntzOjI6IklEIjtzOjQ6IjMxNTMiO3M6NDoiVVNFUiI7czoyMDoiYVc1bWIwQnlkbTB0YzNCaUxuSjEiO3M6NjoiUkFORE9NIjtzOjg6Ijd6cjlxY3ZsIjtzOjE1OiJJTkZPRkxPVC1BUElLRVkiO3M6NDA6ImJmNWVlNWU0ZmJiNTJhMzIzZTQyOWMxODU0ZmJlZTVhODY4NWY1ZWQiO30='
+const selectedTab = ref<number>(0);
+
+const tabs = ['Водные круизы', 'РЖД круизы'];
+
+const widgetDataId = ref(
+    'YTo0OntzOjI6IklEIjtzOjQ6IjMxNTMiO3M6NDoiVVNFUiI7czoyMDoiYVc1bWIwQnlkbTB0YzNCaUxuSjEiO3M6NjoiUkFORE9NIjtzOjg6IjI0YmJpMjd2IjtzOjE1OiJJTkZPRkxPVC1BUElLRVkiO3M6NDA6ImJmNWVlNWU0ZmJiNTJhMzIzZTQyOWMxODU0ZmJlZTVhODY4NWY1ZWQiO30='
 );
 
 onMounted(async () => {
@@ -28,23 +30,23 @@ onMounted(async () => {
         script.async = true;
         script.innerHTML = `
         (function(d,w){
-        var h=d.getElementsByTagName("script")[0];
-        s=d.createElement("script");
-        s.src="https://bitrix.infoflot.com/local/templates/infoflot/frontend/js/infoflotIframe.js";
-        s.async=true;
-        s.onload=function(){
+            var h=d.getElementsByTagName("script")[0];
+            s=d.createElement("script");
+            s.src="https://bitrix.infoflot.com/local/templates/infoflot/frontend/js/infoflotIframe.js";
+            s.async=true;
+            s.onload=function(){
             w.createInfoflotWidget("https://bitrix.infoflot.com/rest/api/search.filter/",{
-            key: "${widgetDataId.value}",
-            referer: encodeURIComponent(location.href)
+                key: "${widgetDataId.value}",
+                referer: encodeURIComponent(location.href)
             });
-        };
-        h.parentNode.insertBefore(s,h);
+            };
+            h.parentNode.insertBefore(s,h);
         })(document,window);
-    `;
+        `;
         document.body.appendChild(script);
     }
 
-    await getCategory(id);
+    await getCategory(id as number);
 });
 </script>
 
@@ -52,43 +54,57 @@ onMounted(async () => {
     <div class="mt-[130px] l:mt-[155px] mb-[104px] px-10">
         <BaseReturn text="Назад" />
 
-        <div
-            v-if="category?.data.length"
-            class="list"
-        >
-            <SlideItem
-                v-for="tour in category?.data"
-                :id="tour.id"
-                :key="tour.id"
-                :title="tour.attributes.title"
-                :text="tour.attributes.subtitle"
-                :url="tour.attributes.cover.data?.attributes.url"
-                @click="
-                    router.push({
-                        name: 'tour',
-                        params: {
-                            id: tour.id,
-                        },
-                    })
-                "
-            />
+        <div v-if="category?.data.length && id !== 7">
+            <div class="list">
+                <SlideItem
+                    v-for="tour in category.data"
+                    :id="tour.id"
+                    :key="tour.id"
+                    :title="tour.attributes.title"
+                    :text="tour.attributes.subtitle"
+                    :url="tour.attributes.cover.data?.attributes.url"
+                    @click="
+                        router.push({
+                            name: 'tour',
+                            params: { id: tour.id },
+                        })
+                    "
+                />
+            </div>
         </div>
 
-        <!-- ToDo: переделать -->
         <div v-if="id === 7">
-            <div class="tabs">
-                <div class="tab">Водные круизы</div>
-                <div class="tab">РЖД круизы</div>
+            <div class="tabs max-w-[1200px] mx-auto">
+                <BaseTypography
+                    v-for="(tab, idx) in tabs"
+                    :key="idx"
+                    :text="tab"
+                    type="subtitle3"
+                    class="cursor-pointer"
+                    @click="selectedTab = idx"
+                    :class="{ active: selectedTab === idx }"
+                />
             </div>
 
-            <div
-                class="infoflotWidget"
-                :data-id="widgetDataId"
-                :data-index="1"
-            />
+            <div v-show="selectedTab === 0">
+                <div
+                    class="infoflotWidget"
+                    :data-id="widgetDataId"
+                    :data-index="1"
+                />
+            </div>
+
+            <div v-show="selectedTab === 1">
+                <div class="max-w-[1200px] mx-auto">
+                    <BaseTypography
+                        text="Страница в разработке"
+                        :type="isMobile ? 'subtitle2-m' : 'subtitle2'"
+                    />
+                </div>
+            </div>
         </div>
 
-        <div v-else>
+        <div v-if="!category?.data.length && id !== 7">
             <BaseTypography
                 text="Страница в разработке"
                 :type="isMobile ? 'subtitle2-m' : 'subtitle2'"
@@ -102,7 +118,14 @@ onMounted(async () => {
     @apply flex flex-wrap justify-center gap-y-[56px] gap-x-[104px] max-w-[1126px] mx-auto l:grid l:grid-cols-[428px_428px] l:mb-[104px];
 }
 
-iframe {
-    width: 1100px !important;
+.tabs {
+    display: flex;
+    gap: 40px;
+    margin-bottom: 20px;
+}
+
+.active {
+    color: var(--primary-color);
+    border-bottom: 3px solid var(--active-color);
 }
 </style>
